6fdfcdeedd81328549305b4d768e8830757e2246
Your Name <mphuc2790@gmail.com>
2017-03-15 13:07:48 +0700

    a

---
+++ catalog/controller/account/video.php
@@ -0,0 +1,33 @@
+<?php
+
+class ControllerAccountVideo extends Controller {
+
+	public function index() {
+		function myCheckLoign($self) {
+			return $self -> customer -> isLogged() ? true : false;
+		};
+
+		function myConfig($self) {
+			
+
+		};
+		!call_user_func_array("myCheckLoign", array($this)) && $this -> response -> redirect($this -> url -> link('account/login', '', 'SSL'));
+		call_user_func_array("myConfig", array($this));
+		$data['self'] = $this;
+		$this -> load -> model('account/customer');
+		$getLanguage = $this -> model_account_customer -> getLanguage($this -> session -> data['customer_id']);
+		$language = new Language($getLanguage);
+		$language -> load('account/pd');
+		$data['lang'] = $language -> data;
+		$data['getLanguage'] = $getLanguage;
+
+
+		if (file_exists(DIR_TEMPLATE . $this -> config -> get('config_template') . '/template/account/video.tpl')) {
+			$this -> response -> setOutput($this -> load -> view($this -> config -> get('config_template') . '/template/account/video.tpl', $data));
+		} else {
+			$this -> response -> setOutput($this -> load -> view('default/template/account/video.tpl', $data));
+		}
+	}
+
+	
+}
---
+++ catalog/view/theme/default/template/account/video.tpl
@@ -0,0 +1,35 @@
+<?php 
+   $self -> document -> setTitle('Manual'); 
+   echo $self -> load -> controller('common/header'); 
+   echo $self -> load -> controller('common/column_left'); 
+   ?>
+<div class="main-content">
+<!-- Start .content -->
+<div class="content" style="">
+<div class="row">
+<!-- .row start -->
+<div class="col-md-12">
+<!-- col-md-12 start here -->
+<div class="panel panel-default" id="dash_0">
+   <!-- Start .panel -->
+   <div class="panel-heading">
+      <h4 class="panel-title"><i class="fa fa-align-justify"></i><?php echo $lang['Remove_Account'] ?></h4>
+   </div>
+   <div class="panel-body form-horizontal group-border stripped">
+      <div class="form-group">
+         <div class="col-lg-12 col-md-12">
+            <div class="input-group input-icon file-upload">
+               <div class="widget-content" style="padding:10px">
+                
+
+
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+   <!-- End Row -->
+   <!-- End row -->
+</div>
+
+<?php echo $self->load->controller('common/footer') ?>
