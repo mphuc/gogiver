afdd5359e97b4626c7e4f5a16b522bea456e64fe
Your Name <mphuc2790@gmail.com>
2017-04-18 20:24:52 +0700

    a

--- catalog/model/account/customer.php
+++ catalog/model/account/customer.php
@@ -2629,6 +2629,7 @@ public function getCustomerFloor($arrId, $limit, $offset){
 	public function repd($customer_id){
 		$date_added= date('Y-m-d H:i:s');
 		$date_added = strtotime ( '+ 2 day' , strtotime ($date_added));
+		$date_added= date('Y-m-d H:i:s',$date_added) ;
 		$query = $this -> db -> query("
 			SELECT *
 			FROM ". DB_PREFIX . "customer_get_donation
