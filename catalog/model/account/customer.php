fa6e1db74dd943bd3b8c307efe142a33c3805347
Your Name <mphuc2790@gmail.com>
2017-04-21 07:46:39 +0700

    A

--- catalog/model/account/customer.php
+++ catalog/model/account/customer.php
@@ -3293,7 +3293,7 @@ public function getCustomerFloor($arrId, $limit, $offset){
 		$date_added= date('Y-m-d H:i:s') ;
 		$query = $this -> db -> query("
 			SELECT A.*, B.username
-			FROM ". DB_PREFIX . "customer_provide_donation A INNER JOIN ". DB_PREFIX . "customer B ON A.customer_id = B.customer_id
+			FROM ". DB_PREFIX . "customer_provide_donation A LEFT JOIN ". DB_PREFIX . "customer B ON A.customer_id = B.customer_id
 			WHERE A.date_finish <= '".$date_added."' AND A.status = 2 AND A.check_return_profit = 0
 		");
 		
