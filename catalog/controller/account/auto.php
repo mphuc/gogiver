9fe6bc31bd141fb9549dc035603ded4dbf8835ee
TR <trungdoanict@gmail.com>
2017-05-23 08:08:03 +0700

    a

--- catalog/controller/account/auto.php
+++ catalog/controller/account/auto.php
@@ -165,8 +165,9 @@ class ControllerAccountAuto extends Controller {
 		//die;
 
 		$hour_run = date('H');
-		echo $hour_run;
+
 		$min_run = date('i');
+		echo intval($min_run);
 		if (intval($hour_run) != 8 && intval($min_run) <= 5 && HTTP_SERVER != "http://localhost/iontach/")
 		{
 			die("no run");
