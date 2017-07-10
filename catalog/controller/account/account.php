fe4543505d9cf542d2683cdc4963c2a07d238438
TR <trungdoanict@gmail.com>
2017-07-10 08:20:23 +0700

    a

--- catalog/controller/account/account.php
+++ catalog/controller/account/account.php
@@ -674,7 +674,11 @@ class ControllerAccountAccount extends Controller {
 		{
 		  	$middle_line = "Manhnhanthinh";
 		}
-
+		if (in_array(34, $big_upline))
+		{
+		  	$middle_line = "nhiem63";
+		}
+		
 		$json['middleline'] = $middle_line;
 		$count = count($big_upline);
 		
